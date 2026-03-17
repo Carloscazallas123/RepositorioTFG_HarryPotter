7107b8db1233eba78a8f2adc1e2e32135ea3d63c
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-03-17 19:04:09 +0100

    feat (Spring): Creacion del DTO completo del usuario UsuarioFullDTO

--- SpringTool/HarryPotter/src/main/java/com/harry/paquetes/dtos/usuario/UsuarioFullDTO.java
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/dtos/usuario/UsuarioFullDTO.java
@@ -1,5 +1,87 @@
 package com.harry.paquetes.dtos.usuario;
+//Este DTO es al momento de que termine de Iniciar Sesion
+//El controlador devolverá este DTO para guardarlo en el Token
+import java.util.*;
 
 public class UsuarioFullDTO {
+	
+	private int idusuario;
+	private String nombre;
+	private String correo;
+	private String casa;
+	private int puntos;
+	private List<Integer>inventario;
+	
+	
+	public UsuarioFullDTO(int idusuario, String nombre, String correo, String casa, int puntos,
+			List<Integer> inventario) {
+		this.idusuario = idusuario;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.casa = casa;
+		this.puntos = puntos;
+		this.inventario = inventario;
+	}
 
+
+	public int getIdusuario() {
+		return idusuario;
+	}
+
+
+	public void setIdusuario(int idusuario) {
+		this.idusuario = idusuario;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getCorreo() {
+		return correo;
+	}
+
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+
+	public String getCasa() {
+		return casa;
+	}
+
+
+	public void setCasa(String casa) {
+		this.casa = casa;
+	}
+
+
+	public int getPuntos() {
+		return puntos;
+	}
+
+
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+
+
+	public List<Integer> getInventario() {
+		return inventario;
+	}
+
+
+	public void setInventario(List<Integer> inventario) {
+		this.inventario = inventario;
+	}
+	
+	
 }
