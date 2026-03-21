12a91fd942a995ef3470783a226cdbdf87f9a145
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-03-21 17:11:51 +0100

    feat (Spring): Creacion del DTO ObjetoFullDTO

---
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/dtos/objeto/ObjetoFullDTO.java
@@ -0,0 +1,72 @@
+package com.harry.paquetes.dtos.objeto;
+
+public class ObjetoFullDTO {
+
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private int costo;
+	private String personaje;
+	
+	
+	public ObjetoFullDTO(int id, String nombre, String descripcion, int costo, String personaje) {
+		
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.costo = costo;
+		this.personaje = personaje;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+	public int getCosto() {
+		return costo;
+	}
+
+
+	public void setCosto(int costo) {
+		this.costo = costo;
+	}
+
+
+	public String getPersonaje() {
+		return personaje;
+	}
+
+
+	public void setPersonaje(String personaje) {
+		this.personaje = personaje;
+	}
+	
+	
+}
