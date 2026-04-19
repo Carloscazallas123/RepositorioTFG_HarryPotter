7d806435fb2f90bccfb6585f32a8af893f9f0837
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-04-19 15:47:41 +0200

    feat (Spring): Creacion del RepoUsuario

---
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/repositorios/RepoUsuarios.java
@@ -0,0 +1,20 @@
+package com.harry.paquetes.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.harry.paquetes.entidades.*;
+@Repository
+public interface RepoUsuarios extends JpaRepository<UsuarioEntity, Integer> {
+
+	//Consulta para obtener todos los usuarios
+	@Query("SELECT FROM ClienteEntity")
+    List<UsuarioEntity> ObtenerAllUsuarios();
+	
+	//Consulta para obtener un usuario por su nombre y contraseña
+	@Query("SELECT FROM ClienteEntity c WHERE c.nombre=:nombre AND c.contrasena=:contrasena")
+	UsuarioEntity ObtenerAllUsuarios(@Param("nombre")String nombre, @Param("contrasena") String contrasena);
+}
