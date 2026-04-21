b5795f34655057ce4d9faf2042795dcb3a92cc7b
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-04-21 16:21:52 +0200

    feat (Spring): Creacion del RepoPersonajes

--- SpringTool/HarryPotter/src/main/java/com/harry/paquetes/repositorios/RepoPersonajes.java
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/repositorios/RepoPersonajes.java
@@ -1,5 +1,5 @@
 package com.harry.paquetes.repositorios;
-
+//Repositorio para la entidad PersonajeEntity
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
