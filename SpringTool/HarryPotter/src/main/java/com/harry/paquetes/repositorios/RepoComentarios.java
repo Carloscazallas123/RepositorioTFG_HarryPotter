7182c878a411b9e05632d53378896e0fe95cfd05
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-04-22 12:37:09 +0200

    fix (Spring): Pequeños arreglos en los Repositorios. Fixes #30

--- SpringTool/HarryPotter/src/main/java/com/harry/paquetes/repositorios/RepoComentarios.java
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/repositorios/RepoComentarios.java
@@ -1,5 +1,21 @@
 package com.harry.paquetes.repositorios;
+//Repositorio para la entidad CometnariosEntity
+import java.util.List;
 
-public class RepoComentarios {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.harry.paquetes.entidades.*;
+@Repository
+public interface RepoComentarios extends JpaRepository<ComentarioEntity, Integer> {
 
+	//Consulta para obtener todos los Comentarios
+	@Query("SELECT c FROM ComentarioEntity c")
+    List<ComentarioEntity> ObtenerAllComentarios();
+	
+	//Consulta para obtener un comentario a traves del Id del Usuario
+	@Query("SELECT c FROM ComentarioEntity c WHERE c.usuario.idusuario=:id")
+	ComentarioEntity ObtenerPorUsuarioID(@Param("id")int id);
 }
+
