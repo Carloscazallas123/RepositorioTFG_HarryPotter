a22626257af71617ba5d07f31f465bfe5514639d
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-04-27 19:14:26 +0200

    feat (Spring): Creacion del Controlador de los Comentarios

--- SpringTool/HarryPotter/src/main/java/com/harry/paquetes/dtos/comentario/ComentarDTO.java
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/dtos/comentario/ComentarDTO.java
@@ -7,12 +7,23 @@ public class ComentarDTO {
 	private String Descripcion;
 	private int valor;
 	private Date fecha;
+	private int id;
 	
-	public ComentarDTO(String descripcion, int valor, Date fecha) {
+	public ComentarDTO(String descripcion, int valor, Date fecha, int id) {
 
 		Descripcion = descripcion;
 		this.valor = valor;
 		this.fecha = fecha;
+		this.id = id;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getDescripcion() {
