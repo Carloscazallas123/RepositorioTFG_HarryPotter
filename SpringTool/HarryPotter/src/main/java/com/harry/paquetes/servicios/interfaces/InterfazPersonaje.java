ffddb61e3d3fe9c01dffb1acac84172cd15790aa
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-04-28 19:30:15 +0200

    feat (Spring): Creacion de la interfaz de los Comentarios

--- SpringTool/HarryPotter/src/main/java/com/harry/paquetes/servicios/interfaces/InterfazPersonaje.java
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/servicios/interfaces/InterfazPersonaje.java
@@ -5,6 +5,7 @@ import com.harry.paquetes.dtos.personajes.PersonajeVistDTO;
 import com.harry.paquetes.dtos.usuario.UsuarioFullDTO;
 import java.util.*;
 
+//Interfaz para la implementacion de los Personajes
 public interface InterfazPersonaje {
 
 	//Metodo para mostar los personajes desbloqueados por el usuario
