1e65608ecab21fe2dac62c33e06588c05e870c49
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-04-28 19:19:29 +0200

    feat (Spring): Creacion de la interfaz de los Personajes

--- SpringTool/HarryPotter/src/main/java/com/harry/paquetes/servicios/interfaces/InterfazPersonaje.java
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/servicios/interfaces/InterfazPersonaje.java
@@ -1,5 +1,15 @@
 package com.harry.paquetes.servicios.interfaces;
 
-public class InterfazPersonaje {
+import com.harry.paquetes.dtos.personajes.PersonajeMostDTO;
+import com.harry.paquetes.dtos.personajes.PersonajeVistDTO;
+import com.harry.paquetes.dtos.usuario.UsuarioFullDTO;
+import java.util.*;
 
+public interface InterfazPersonaje {
+
+	//Metodo para mostar los personajes desbloqueados por el usuario
+	List<PersonajeMostDTO>mostrarbesbloqueados(UsuarioFullDTO usuario);
+	
+	//Metodo para mostrar el personaje seleccionado
+	PersonajeVistDTO mostrarpersonaje(int id);
 }
--- SpringTool/HarryPotter/src/main/java/com/harry/paquetes/servicios/interfaces/InterfazUsuario.java
+++ SpringTool/HarryPotter/src/main/java/com/harry/paquetes/servicios/interfaces/InterfazUsuario.java
@@ -1,5 +1,15 @@
 package com.harry.paquetes.servicios.interfaces;
 
-public class InterfazUsuario {
+import com.harry.paquetes.dtos.usuario.RegistroDTO;
+import com.harry.paquetes.dtos.usuario.SesionDTO;
+import com.harry.paquetes.dtos.usuario.UsuarioFullDTO;
 
+//Interfaz para la implementacion de los Usuarios
+public interface InterfazUsuario {
+
+	//Metodo para registrar el usuario
+	UsuarioFullDTO registrarusuario(RegistroDTO registro);
+	
+	//Metodo para iniciar sesion del usuario
+	UsuarioFullDTO inicarsesionusuario(SesionDTO sesion);
 }
